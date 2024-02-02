684366fd7bd59f1351303b3af2b1012f8bc94c37
Smail-Bel <132350771+Smail-Bel@users.noreply.github.com>
2024-02-02 19:50:50 +0100

    Add files via upload

--- Streamlit_App_Final_3.py
+++ Streamlit_App_Final_3.py
@@ -1,14 +1,21 @@
-import streamlit as st
-from PIL import Image
-import tensorflow as tf
 from PIL import Image as PilImage
-import numpy as np
+import streamlit as st
+import tensorflow as tf
 
-# Streamlit app
-st.title("Cat or Dog Classifier")
+# Function to preprocess the image
+def preprocess_image(image_path):
+    pil_image = PilImage.open(image_path)
+    pil_image_resized = pil_image.resize((224, 224))
+    img_array = tf.keras.preprocessing.image.img_to_array(pil_image_resized)
+    img_array = tf.expand_dims(img_array, 0)  # Create batch axis
+    img_array /= 255.0  # Normalize pixel values to [0, 1]
+    return img_array
 
-# Upload image through Streamlit
-uploaded_file = st.file_uploader("Choose an image...", type="jpg")
+# Main Streamlit code
+st.title("Dog vs Cat Image Classifier")
+
+# Upload an image through Streamlit
+uploaded_file = st.file_uploader("Choose a file", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None:
     try:
@@ -16,14 +23,11 @@ if uploaded_file is not None:
         pil_image = PilImage.open(uploaded_file)
         st.image(pil_image, caption="Uploaded Image", use_column_width=True)
 
+        # Preprocess the resized image for the model
+        img_array = preprocess_image(uploaded_file)
+
         # Load the pre-trained model only if an image is uploaded
         model = tf.keras.models.load_model('dog_cat_detector_model_Final_2.h5')
-
-        # Preprocess the image for the model
-        img_array = tf.keras.preprocessing.image.img_to_array(pil_image)
-        img_array = tf.image.resize(img_array, (224, 224))  # Resize to match the model's input shape
-        img_array = tf.expand_dims(img_array, 0)  # Create batch axis
-        img_array /= 255.0  # Normalize pixel values to [0, 1]
 
         # Make predictions using the loaded model
         predictions = model.predict(img_array)
@@ -42,4 +46,4 @@ if uploaded_file is not None:
         st.write(f"Confidence: {float(confidence):.2%}")
 
     except Exception as e:
-        st.error(f"Errort processing the image: {e}")
+        st.error(f"Error processing the image: {e}")
